404897adb21a3fe5cb2fd659bf3f6822776fcace
JedPogi <mangjuan0812@gmail.com>
2025-02-04 17:50:15 +0100

    need to fix date form

--- src/app/components/dialog-add-project/dialog-add-project.component.ts
+++ src/app/components/dialog-add-project/dialog-add-project.component.ts
@@ -1,11 +1,27 @@
 import { Component, Inject } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
+import { DateAdapter, MAT_DATE_LOCALE } from '@angular/material/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import {MomentDateAdapter, provideMomentDateAdapter} from '@angular/material-moment-adapter';
+
+const MY_DATE_FORMAT = {
+  parse: {
+    dateInput: 'YYYY/MM/DD', // this is how your date will be parsed from Input
+  },
+  display: {
+    dateInput: 'YYYY/MM/DD', // this is how your date will get displayed on the Input
+  }
+};
+
 
 @Component({
   selector: 'app-dialog-add-project',
   templateUrl: './dialog-add-project.component.html',
-  styleUrl: './dialog-add-project.component.scss'
+  styleUrl: './dialog-add-project.component.scss',
+  providers: [
+    {provide: MAT_DATE_LOCALE, useValue: "en-GB"},
+    provideMomentDateAdapter(),
+  ],
 })
 export class DialogAddProjectComponent {
   priorita: any;
@@ -16,7 +32,8 @@ export class DialogAddProjectComponent {
       title: new FormControl(""),
       description: new FormControl(""),
       utente: new FormControl(""),
-      priorita: new FormControl("")
+      priorita: new FormControl(""),
+      date: new FormControl("")
     });
 
     this.priorita = [
@@ -31,3 +48,5 @@ export class DialogAddProjectComponent {
     this.dialogRef.close(this.newProject);
   }
 }
+
+
