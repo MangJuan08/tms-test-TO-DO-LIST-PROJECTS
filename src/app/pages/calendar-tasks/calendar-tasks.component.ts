f55f625b7e2eee2093d074fbd6b499a75bc00492
JedPogi <mangjuan0812@gmail.com>
2025-02-04 16:35:45 +0100

    cleaning code

--- src/app/pages/calendar-tasks/calendar-tasks.component.ts
+++ src/app/pages/calendar-tasks/calendar-tasks.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { CalendarOptions } from '@fullcalendar/core'; // useful for typechecking
 import dayGridPlugin from '@fullcalendar/daygrid';
+import { ProjectsService } from '../../services/projects.service';
+import { Projects } from '../../model/projects.model';
 
 @Component({
   selector: 'app-calendar-tasks',
@@ -8,13 +10,45 @@ import dayGridPlugin from '@fullcalendar/daygrid';
   styleUrl: './calendar-tasks.component.scss'
 })
 export class CalendarTasksComponent {
-
+  isLoading: any;
   public calendarOptions: any;
-  constructor() {
+  constructor(private projectService: ProjectsService) {
+    this.isLoading = true;
     this.calendarOptions = {
       initialView: 'dayGridMonth',
       plugins: [dayGridPlugin]
     };
+
+
+    this.projectService.getAllProjects().subscribe((res: Projects[]) => {
+      /*
+            this.newProjects = res.filter((item: any) => {
+              return item.stato == "NUOVO"
+            })
+      
+            this.projectService.newProjectsQuantity.next(this.newProjects.length);
+      
+      
+            this.onProgressProjects = res.filter((item: any) => {
+              return item.stato == "PROGRESS"
+            })
+      
+            this.projectService.onProgressProjectQuantity.next(this.onProgressProjects.length);
+      
+            this.completeProjects = res.filter((item: any) => {
+              return item.stato == "COMPLETATO"
+            })
+      
+            this.projectService.completeProjectQuantity.next(this.completeProjects.length);
+      */
+
+      console.log(res)
+    })
+
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 3000)
+
   }
-  
+
 }
